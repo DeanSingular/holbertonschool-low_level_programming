476aa71e9358c790e31aab7b646e19c9ffa63b45
DeanSingular <DMabuela@singular.co.za>
2024-03-05 10:51:51 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main(void)
-{
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
-}
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c -o program
