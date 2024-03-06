65be8dca9f1774709c8bfa254f8a9c30ba0c35d1
DeanSingular <DMabuela@singular.co.za>
2024-03-06 07:27:23 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,13 @@
 * Return: Always 0 (Success)
 */
 
-int main() 
+int main()
 {
 char c;
-for (c = '0'; c <= '9'; ++c) 
+for (c = '0'; c <= '9'; ++c)
 {
 putchar(c);
 }
 putchar('\n');
-return 0;
+return (0);
 }
