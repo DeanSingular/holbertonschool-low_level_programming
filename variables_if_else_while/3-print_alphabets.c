8b8b786ad3ce21ceb7c9abd206b9de93b96e341c
DeanSingular <DMabuela@singular.co.za>
2024-03-06 05:59:16 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 /**
  * main - Entry point
- *
- * Description: Prints the alphabet in lowercase, then uppercase, 
- * followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char lowercase;
