b1735f20a6797d79472bf21cbfe2f6ac7af5d7ff
DeanSingular <DMabuela@singular.co.za>
2024-03-11 09:21:36 +0000

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * mul - multipies two integers
- * @a: number to be multiplied
- * @b: number to be multiplied
- * Return: return result
- */
+* mul - multipies two integers
+* @a: number to be multiplied
+* @b: number to be multiplied
+* Return: return result
+*/
 int nul(int x, int y)
 {
-	return (x * y);
+return (x * y);
 }
