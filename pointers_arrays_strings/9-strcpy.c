9edbb1970d8c018501ff01329d4c8f94165c366a
DeanSingular <DMabuela@singular.co.za>
2024-03-12 05:34:31 +0000

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -7,23 +7,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
-
-	len = _strlen(src);
-	for (i = 0; i < len; i++)
-		dest[i] = src[i];
-	return (dest);
-}
-/**
- * _strlen - return the length of a string
- * @s:the string that need to find the length
- * Return: return the length of the string
- */
-int _strlen(char *s)
+int i;
+for (i = 0; src[i] != '\0'; i++)
 {
-	int length = 0;
-
-	while (s[length] != '\0')
-		length++;
-	return (length);
+des[i] = src[i];
+}
+dest[i++] = src[i];
+return (dest);
 }
