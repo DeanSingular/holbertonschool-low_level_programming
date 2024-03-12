22049be7ab3d8afe97f9441ec439ff732c5534ea
DeanSingular <DMabuela@singular.co.za>
2024-03-12 05:04:54 +0000

    5-rev_string.c 6-puts2.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - Write a function that reverses a string.
+ * @s: the string that need to reverse
+ */
+void rev_string(char *s)
+{
+	int length = 0, index;
+	int i = 0, len;
+	char tmp;
+
+	while (s[length] != '\0')
+		length++;
+	len = length / 2;
+	for (index = length - 1; index >= len; index--)
+	{
+		tmp = s[index];
+		s[index] = s[i];
+		s[i] = tmp;
+		i++;
+	}
+}
