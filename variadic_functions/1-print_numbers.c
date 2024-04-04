cb7feccf762934703207293d211e5fb3c22e0a6b
DeanSingular <DMabuela@singular.co.za>
2024-04-04 06:48:26 +0000

    1-print_numbers.c 2-print_strings.c 3-print_all.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_numbers - prints number
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num_list;
+	unsigned int i;
+	const char *tmp;
+
+	if (separator == NULL)
+		tmp = "";
+	else
+		tmp = separator;
+	/* initialize num_list for n number of arguments */
+	va_start(num_list, n);
+	/* access all the arguments assigned to num_list */
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num_list, int));
+		if (i < n - 1)
+			printf("%s", tmp);
+	}
+	printf("\n");
+	/* clean memory reserved for num_list */
+	va_end(num_list);
+}
