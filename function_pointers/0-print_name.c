25ffaca3c38d3e337090064fe5d21371596e7569
DeanSingular <DMabuela@singular.co.za>
2024-04-04 06:10:59 +0000

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - print a name
+ * @name: the name that need to print
+ * @f: pointer to function that prints a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
