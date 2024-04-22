efab79733b0b0d18eeff4475c667ee656515af39
DeanSingular <DMabuela@singular.co.za>
2024-04-22 05:40:11 +0000

    holbertonschool-low_level_programmingwq

--- file_io/holberton.h
+++ file_io/holberton.h
@@ -1,20 +1,14 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef HEADER_H
+#define HEADER_H
 
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
- */
-
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-
+#define BUFSIZE 1204
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-#endif /* HOLBERTON_H */
+int _putchar(char c);
+#endif
