554ddd447764c952abf7dd07ec5bfe96979349c6
DeanSingular <DMabuela@singular.co.za>
2024-04-22 06:13:32 +0000

    file_io

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,32 +1,31 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-  * read_textfile - reads a text file and prints it to the POSIX standard out
-  * @filename: name of the file to read
-  * @letters: number of characters to print
-  * Return: 0 on success
-  **/
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int txt_file, total, read_status;
-	char buffer[BUFSIZE];
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-	if (filename == NULL)
+	if (!buff)
 		return (0);
-	txt_file = open(filename, O_RDONLY);
-	if (txt_file == -1)
+
+	if (!filename)
 		return (0);
-	total = 0;
-	read_status = 1;
-	while (letters > BUFSIZE && read_status != 0)
-	{
-		read_status = read(txt_file, buffer, BUFSIZE);
-		write(STDOUT_FILENO, buffer, read_status);
-		total += read_status;
-		letters -= BUFSIZE;
-	}
-	read_status = read(txt_file, buffer, letters);
-	write(STDOUT_FILENO, buffer, read_status);
-	total += read_status;
-	close(txt_file);
-	return (total);
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+
+	free(buff);
+	close(fd);
+	return (readed);
 }
--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,25 +1,46 @@
-#include "holberton.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
-  * append_text_to_file - appends text to a file
-  * @filename: file to append to, if NULL, return -1
-  * @text_content: content to append, if NULL, do not append
-  * Return: 1 on success, -1 on failure
+  * append_text_to_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int append_file, len, wr_stat;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	append_file = open(filename, O_WRONLY | O_APPEND);
-	if (append_file == -1)
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-	for (len = 0; text_content[len]; len++)
-		;
-	wr_stat = write(append_file, text_content, len);
-	close(append_file);
-	return (wr_stat == -1 ? -1 : 1);
+
+	if (text_content)
+	{
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
