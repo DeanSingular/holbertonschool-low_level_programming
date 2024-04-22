1ab6c0daa6ec09d376709c168520c61f3150d455
DeanSingular <DMabuela@singular.co.za>
2024-04-22 06:16:25 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,29 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-  * append_text_to_file - ...
+  * create_file - ...
   * @filename: ...
   * @text_content: ...
   *
   * Return: ...
   */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
-	}
+		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
